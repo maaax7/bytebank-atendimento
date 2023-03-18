735258ca0e41f41f1772c5e96368673c62c1c7f8
maaax7 <maxtuka@gmail.com>
2023-03-18 18:32:18 +0000

    Finalizando estudo da formacao c# orientacao a objetos

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,5 @@
-﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Atendimento;
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+new ByteBankAtendimento().AtendimentoCliente();
 
